8878ca9fdbea168a0043c0c672b2316694750235
Morgen Diaz <morgen@webslinger.works>
2022-10-03 14:50:54 -1000

    Can search quotes by content.

--- src/routes/view-quollection/ViewQuollection.jsx
+++ src/routes/view-quollection/ViewQuollection.jsx
@@ -1,18 +1,40 @@
+import { useEffect, useState } from "react";
 import useQuotes from "../../hooks/useQuotes";
 import QuoteSummaryList from "./QuoteSummaryList";
 
 function ViewQuollection() {
   const quotes = useQuotes();
+  const [filteredQuotes, setFilteredQuotes] = useState([]);
+
+  useEffect(() => {
+    setFilteredQuotes([...quotes]);
+  }, [quotes]);
+
+  const handleSearchInputChanged = (event) => {
+    const searchText = event.target.value.toLowerCase();
+
+    setFilteredQuotes(
+      quotes.filter((quote) => {
+        const searchableContent = quote.content.toLowerCase();
+        return searchableContent.includes(searchText);
+      })
+    );
+  };
 
   return (
     <div className="flex flex-col items-stretch h-full gap-2 p-4 text-left ">
-      <input type="text" placeholder="Search Quollection" className="p-3" />
+      <input
+        onChange={handleSearchInputChanged}
+        type="text"
+        placeholder="Search Quollection"
+        className="p-3"
+      />
 
       <h2 className="text-gray-700">Filter by tag</h2>
 
       <button className="self-start mb-4">Add Tag</button>
 
-      <QuoteSummaryList quotes={quotes} />
+      <QuoteSummaryList quotes={filteredQuotes} />
     </div>
   );
 }
